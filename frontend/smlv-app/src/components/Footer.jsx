8aa24a9d348acb18706d00dc0cebb3516bd69d51
antaler <antaler.dev@outlook.com>
2024-06-02 20:51:11 +0200

    RESET

--- frontend/smlv-app/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export function Footer () {
-  return (
-    <footer className='py-4 font-geist grid place-items-center w-full  text-slate-200'>
-      <p>Made with love 🧡 by <a className='cursor-pointer hover:opacity-50 transition-opacity ' href='http://antaler.com'>antaler</a></p>
-    </footer>
-  )
-}
